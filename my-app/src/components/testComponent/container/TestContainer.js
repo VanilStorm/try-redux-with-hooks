8d6a82e7140e3c12b67747094c797c69bea88e6d
VanilStorm <vanil.work@mail.ru>
2022-02-04 16:49:04 +0300

    create react app

---
+++ my-app/src/components/testComponent/container/TestContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TestComponent from "../component/TestComponent";
+
+const TestContainer = () => {
+    return (
+        <div>
+            <TestComponent/>
+        </div>
+    );
+};
+
+export default TestContainer;
---
+++ my-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestContainer from "./components/testComponent/container/TestContainer";
+
+ReactDOM.render(
+    <TestContainer/>,
+    document.getElementById('root')
+);
